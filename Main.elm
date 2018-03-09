d58dfe627cdcee46b2583bc47a59e4adafe32b20
Joonatan Saarhelo <joon.saar@gmail.com>
2018-03-09 19:39:42 +0200

    piirrä myös kuolleet ruudut ja näytä hieman ruutuja elävien ympäriltä

--- Main.elm
+++ Main.elm
@@ -7,10 +7,10 @@ import Color
 
 
 main =
-    tyhja
+    tyhjä
         |> laitaRuutuun ( 0, 0 )
         |> laitaRuutuun ( 3, 2 )
-        |> piirraRuudukko 10
+        |> piirräRuudukko 10
         |> Collage.Render.svg
 
 
@@ -22,8 +22,8 @@ type alias Ruudukko =
     Set Piste
 
 
-tyhja : Ruudukko
-tyhja =
+tyhjä : Ruudukko
+tyhjä =
     Set.empty
 
 
@@ -37,17 +37,54 @@ poistaRuudusta paikka =
     Set.remove paikka
 
 
-piirraRuudukko : Float -> Ruudukko -> Collage msg
-piirraRuudukko mittakaava =
-    Set.toList
-        >> List.map
-            (\( x, y ) ->
-                shift ( mittakaava * toFloat x, mittakaava * toFloat y ) (elava mittakaava)
-            )
-        >> group
+piirräRuudukko : Float -> Ruudukko -> Collage msg
+piirräRuudukko mittakaava ruudukko =
+    let
+        elävät =
+            Set.toList ruudukko
+
+        ( äksät, yyt ) =
+            List.unzip elävät
+
+        vaihteluväli x =
+            List.range
+                ((List.minimum x |> Maybe.withDefault 0) - tyhjääNäytetään)
+                ((List.maximum x |> Maybe.withDefault 0) + tyhjääNäytetään)
+
+        -- montako tyhjää ruutua näytetään elävän alueen ympäriltä
+        tyhjääNäytetään =
+            5
+    in
+        -- leksikografinen järjestys, jotta elävät olisivat
+        -- samassa järjestyksessä kuin nämä
+        vaihteluväli äksät
+            |> List.concatMap (\x -> List.map ((,) x) (vaihteluväli yyt))
+            |> List.foldl
+                (\paikka ( elävät, ulos ) ->
+                    let
+                        kuollut =
+                            ( elävät, väriksi Color.black )
+
+                        väriksi väri =
+                            piirräRuutu mittakaava paikka väri :: ulos
+                    in
+                        case elävät of
+                            eka :: loput ->
+                                if eka == paikka then
+                                    ( loput, väriksi Color.green )
+                                else
+                                    kuollut
+
+                            [] ->
+                                kuollut
+                )
+                ( elävät, [] )
+            |> Tuple.second
+            |> group
 
 
-elava : Float -> Collage msg
-elava mittakaava =
+piirräRuutu : Float -> Piste -> Color.Color -> Collage msg
+piirräRuutu mittakaava ( x, y ) väri =
     square mittakaava
-        |> filled (uniform Color.green)
+        |> filled (uniform väri)
+        |> shift ( mittakaava * toFloat x, mittakaava * toFloat y )
