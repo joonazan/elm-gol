a67e252fac9c51c1ecb6c2d02fad18627d6d6d10
Eevan <joon.saar@gmail.com>
2018-03-19 22:52:07 +0200

    Eevan korjaukset

--- src/Life.elm
+++ src/Life.elm
@@ -8,10 +8,10 @@ lukumäärä kertoo kuinka moni kahdeksasta ympäröivästä ruudusta on elossa.
 
 Seuraava kuva piirretään näin:
 
-  - Jos ruudulla oli tasan kolme naapuria, se herää henkiin.
-  - Jos ruudulla oli kaksi naapuria tai kolme naapuria ja se oli elossa
-    , se selviää.
-  - Muulloin ruutu kuolee tai pysyy kuolleena.
+  - Jos ruutu ei ole elossa ja sillä on tasan kolme naapuria, se herää henkiin.
+  - Jos ruutu on jo elossa ja sillä on kaksi tai kolme elävää naapuria
+    , se pysyy elossa.
+  - Muutoin ruutu kuolee tai pysyy kuolleena.
 
 Teemme *Lifeä* simuloivan ohjelman, jossa käyttäjä voi herättää henkiin tai tappaa
 soluja mielensä mukaan ja viedä simulaatiota eteenpäin painamalla nappia.
@@ -25,7 +25,7 @@ soluja mielensä mukaan ja viedä simulaatiota eteenpäin painamalla nappia.
 
 @docs Ruudukko
 
-Settiin voi tallentaa järjestämättömän joukon uniikkeja arvoja.
+Set vastaa matematiikan joukkoa. Settiin voi tallentaa järjestämättömän joukon uniikkeja arvoja.
 Se on tähän oikea valinta, sillä ruutujen järjestyksellä ei ole väliä,
 emmekä halua tallentaa samaa ruutua moneen kertaan.
 
@@ -57,19 +57,33 @@ Tätä funktiota koodatessa kannattaa hyödyntää funktioita `piirraRuutu` ja `
 
 `piirraRuutu` ja `piirraRuudukko` referensoivat paluuarvossaan tyyppiä `Viesti`,
 jota emme vielä tässä vaiheessa määrittele. Tämän voi korjata esim. muuttamalla
-paluuarvoksi `Collage a`.
+funktioiden paluuarvoksi `Collage a`.
 
 @docs piirraRuutu, mittakaava
+
+Funktio `piirraRuutu` saa argumenttina tiedon siitä, onko ruutu elossa. Mistä se tieto
+ruudun elossa olemisesta saadaan, kun sitä ei ole erikseen talletettu mihinkään?
+
+**Vinkki:** Edellä mainittiin, että `Set Ruudukko` sisältää elävien ruutujen koordinaatit.
+`Set`-moduulin funktio `member` kertoo, onko jokin tietty arvo annetussa setissä.
+
 @docs nakyvatRuudut
 
-Esimerkissä näkymä säädetään niin, että kaikki elävät solut näkyvät,
-mutta alkuun vakiokokoinen näkymä on aivan riittävä.
-Voit kopioida koodiisi seuraavanlaisen toteutuksen.
+Voit määritellä alkuun näkyväksi alueeksi 20 x 20 ruutua, joiden koordinaatit vaihtelevat välillä `[0,20)`.
+Kopioi silloin koodiisi alla oleva toteutus.
 
     nakyvatRuudut _ =
         karteesinenTulo (List.range 0 20) (List.range 0 20)
 
+Kun elossa olevien ruutujen ympärille syntyy uusia soluja, osa niistä voi joutua näkymän
+ulkopuolelle. Siksi esimerkkiratkaisussa näkyvyysaluetta säädetään niin,
+että siihen mahtuvat kaikki elävät solut sekä sopivasti tilaa niiden ympärille. Voit
+myöhemmin katsoa toteutuksen ohjelmalistauksesta ja lisätä sen omaan ohjelmaasi.
+
 @docs karteesinenTulo
+
+Karteesisen tulon toteuttamiseen List.concatMap on näppärä. **Tee karteesisesta tulosta
+havainnollistava selostus.**
 
 
 # 2. Solujen lisääminen hiirellä
@@ -96,8 +110,9 @@ Tehdään niille mahdollisimman yksinkertaiset toteutukset.
 
 @docs view
 
-    view =
-        piirraRuudukko
+    view ruudukko =
+        piirraRuudukko ruudukko
+            |> Collage.Render.svg
 
 @docs update
 
@@ -118,12 +133,16 @@ lähettää viestin `Poista` klikatessa ja kuollut viestin `Lisää`.
 
 ## Viestien käsitteleminen
 
-Tällä hetkellä update kuittaa kaikki viestit palauttamalla ruudukon muuttamattomana.
+Tällä hetkellä `update` kuittaa kaikki viestit palauttamalla ruudukon muuttamattomana.
 Laita se lisäämään ja tappamaan soluja viestien saapuessa!
 
-Nyt tarvitset funktion `lisääRuutuun` vastakappaleen.
+Nyt tarvitset funktion `lisääRuutuun` vastaoperaation.
 
 @docs poistaRuudusta
+
+Lisää `update`-funktioon
+oma haaransa myös viestille Simuloi. Sen ei tässä vaiheessa tarvitse tehdä mitään,
+riittää, että se palauttaa muuttumattoman ruudukon.
 
 
 # 3. Simulointi
@@ -135,29 +154,44 @@ Ruudukon ja napin saa aseteltua vierekkäin Collage.Layout-kirjaston funktioilla
 
 @docs simuloi
 
+Päivitä tässä vaiheessa `update`-funktiota niin, että viesti Simuloi
+käynnistää funktion `simuloi`.
+
 
 ## Naapurien laskeminen
 
 Naapurisolut ovat ne, joiden x-, y- tai molemmat koordinaatit eroavat yhdellä
-tarkasteltavasta ruudusta, eli esim. `(x+1, y)` tai `(x+1, y+1)`.
+tarkasteltavasta ruudusta (x, y), eli esim. `(x+1, y)` tai `(x+1, y+1)`.
 
 Jos esimerkin lailla avaruutesi on ääretön kaikkiin suuntiin, kaikkia ruutuja
-ei voi käydä läpi. Sen sijaan voit käydä läpi kaikki elossa olevat solut
-ja lisätä ympäröivien ruutujen naapurien määrää yhdellä.
-Dict on tähän näppärä.
+ei voi käydä läpi. Sen sijaan voit käydä läpi kaikki elossa olevat solut.
+Kun määrität yksi kerrallaan jokaisen elossa olevan solun ympärillä sijaitsevien
+kahdeksan ruudun koordinaatit ja lisäät niistä jokaiselle yhden naapurin
+lisää, sinulla on koossa kaikki pelin sääntöjen soveltamiseen vaadittu tieto.
+
+Dict on näppärä tietorakenne koordinaattien ja niiden naapurien
+määrän tallettamiseen.
+
+Dict on Setin kaltainen tietorakenne, jossa alkioiden
+järjestyksellä ei ole väliä. Dict sisältää avain-arvo-pareja. Meidän
+ohjelmassamme avaimet ovat koordinaatteja (x, y) ja arvot naapurien
+lukumääriä. Dict’istä voidaan avaimen perusteella hakea sitä vastaava arvo.
 
 
 ## Elävien määrittäminen
 
-Kun naapurien laskennan tulokset suodattaa funktiolla `rule`,
-jäljelle jäävät elävät solut.
+Kun naapurien laskennan tulokset, eli Dictin, joka sisältää pareja
+koordinaatti - naapurien määrä, suodattaa funktiolla `rule`,
+jäljelle jäävät pelin seuraavan sukupolven elävät solut.
 
 @docs rule
 
 
 # Testaa tekelettäsi!
 
-giffejä oikeasta toiminnasta
+Alla on aloituskuvioita, jotka tuottavat mielenkiintoisia tuloksia.
+
+    LISÄÄ KUVIA
 
 
 # Jatkokehitystä
@@ -182,8 +216,7 @@ import Html
 import Set exposing (Set)
 
 
-{-| Valmiin ohjelman entry point.
--}
+{-| -}
 main : Program Never Ruudukko Viesti
 main =
     Html.beginnerProgram
@@ -333,7 +366,7 @@ piirraRuudukko ruudukko =
 
 
 {-| Piirtää yhden solun. Ensimmäinen argumentti kertoo solun paikan.
-Toinen kertoo onko se elossa.
+Toinen kertoo onko se elossa. Jos ruutu on elossa, se piirretään vihreänä, jos taas ei, mustana.
 -}
 piirraRuutu : Piste -> Bool -> Collage Viesti
 piirraRuutu (( x, y ) as paikka) elossa =
@@ -350,7 +383,8 @@ piirraRuutu (( x, y ) as paikka) elossa =
             |> onClick (toiminto paikka)
 
 
-{-| Palauttaa listan koordinaateista, jotka tulisi näyttää.
+{-| Palauttaa listan kaikista ruudulle piirrettävistä pisteistä,
+riippumatta siitä, ovatko ne eläviä vai kuolleita.
 -}
 nakyvatRuudut : Ruudukko -> List Piste
 nakyvatRuudut ruudukko =
